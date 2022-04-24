dc06ef5c95ca41335345fd52700f551332128ca0
BASSIROU Biteye <bassbiteye45@gmail.com>
2022-04-24 05:04:35 +0000

    Update feed-service.service.ts

--- src/app/feed-service.service.ts
+++ src/app/feed-service.service.ts
@@ -5,10 +5,9 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class FeedServiceService {
-   url = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.lemonde.fr%2Frss%2Fen_continu.xml";
-   url2 ="http://localhost:8080/api/feed"
+   url ="http://localhost:8080/api/feed"
   constructor(private http: HttpClient) {}
   getFeeds() {
-    return this.http.get(this.url2);
+    return this.http.get(this.url);
   }
 }
